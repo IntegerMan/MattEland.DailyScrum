ee7cdba320d64db83fdcf33d36c2fad5b451d0bc
Matt Eland <Matt.Eland@GMail.com>
2019-12-27 15:05:43 -0500

    Added child binding for server list

--- MattEland.DailyScrum.WinElmish/ElmProcessor.fs
+++ MattEland.DailyScrum.WinElmish/ElmProcessor.fs
@@ -17,6 +17,14 @@ let bindings () : Binding<Model, Msg> list = [
   // One-Way Bindings
   "AppTitle" |> Binding.oneWay (fun _ -> "Daily Scrum by Matt Eland")
   "TurnNumber" |> Binding.oneWay (fun m -> "Turn " + m.Turns.ToString())
+  "Servers" |> Binding.subModelSeq(
+    (fun (m) -> m.Servers),
+    (fun (c) -> c.Id),
+    (fun () -> [
+      "Id" |> Binding.oneWay(fun (_, s) -> s.Id)
+      "Name" |> Binding.oneWay(fun (_, s) -> s.Name)
+    ])
+  )
 
   // Commands
   "NextTurnCommand" |> Binding.cmd NextTurn
--- MattEland.DailyScrum.WinElmish/Model.fs
+++ MattEland.DailyScrum.WinElmish/Model.fs
@@ -1,9 +1,40 @@
 ﻿module MattEland.DailyScrum.WinElmish.Model
 
-type Model = {
-  Turns: int
+type ServerRole = 
+  | Development
+  | QA
+  | Staging
+  | Production
+
+type Server = {
+  Id: int
+  Name: string
+  Role: ServerRole
 }
 
+type Model = {
+  Turns: int
+  Servers: Server list
+}
+
+/// Application default starting state
 let init (): Model = {
   Turns = 0
+  Servers = [
+    {
+      Id=1
+      Name = "SRV-PROD-1"
+      Role = Production
+    }
+    {
+      Id=2
+      Name = "SRV-STAGE-1"
+      Role = Staging
+    }
+    {
+      Id=3
+      Name = "SRV-QA-1"
+      Role = QA
+    }
+  ]
 }

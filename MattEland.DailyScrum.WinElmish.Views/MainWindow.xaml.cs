8fe34ff61eda023dd1e5bd76bc4ade28ea227d86
Matt Eland <Matt.Eland@GMail.com>
2019-12-27 15:58:20 -0500

    Slight UI Tweaks

--- MattEland.DailyScrum.WinElmish.Views/MainWindow.xaml.cs
+++ MattEland.DailyScrum.WinElmish.Views/MainWindow.xaml.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Telerik.Windows.Automation.Peers;
-using Telerik.Windows.Controls;
+﻿using JetBrains.Annotations;
 
 namespace MattEland.DailyScrum.WinElmish.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    [UsedImplicitly]
+    public partial class MainWindow
     {
         public MainWindow()
         {
             //StyleManager.ApplicationTheme = new MaterialTheme();
+            //CrystalPalette.LoadPreset(CrystalPalette.ColorVariation.Dark);
+            //FluentPalette.LoadPreset(FluentPalette.ColorVariation.Dark);
             InitializeComponent();
         }
     }
